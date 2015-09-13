63362bf1dd1b697498b8f22d14989abfe25fc691
Michael Kirkedal Thomsen <kirkedal@acm.org>
2015-09-13 12:24:48 +0200

    Cabal setup

---
+++ RFun.hs
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :  Michael Kirkedal Thomsen, 2013
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :  Michael Kirkedal Thomsen <kirkedal@acm.org>
+-- Stability   :  Running
+-- Portability :
+--
+-- |Main file for interpreting RFun
+--
+-- The language is based on, to which the are make references in the comments:
+--
+--   T. Yokoyama, H. B. Axelsen, and R. Gluck
+--   Towards a reversible functional language
+--   LNCS vol. 7165, pp. 14--29, 2012
+--
+-----------------------------------------------------------------------------
+
+module RFun (main) where
+
+import Main as M
+
+main = M.main
+
+
